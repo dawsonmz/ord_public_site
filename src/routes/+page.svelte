841e8d7bb2bf858d6fc314c416acae144a9dbc4e
its-steff <steffdziadkowiec@proton.me>
2025-05-14 15:13:06 +0200

    add hero to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,22 @@
-<h1 class="text-3xl m-4">Home page</h1>
+<script lang="ts">
+  import heroImage from "$lib/assets/heroImage.png";
+</script>
+
+<section>
+  <img
+    class="w-[300px]"
+    src={heroImage}
+    alt="Person on roller skates with orange helmet"
+  />
+  <h1 class="text-white text-5xl font-bold">
+    RÅ<br />
+    INKLUDERENDE<br />
+    VELORGANISERTE<br />
+  </h1>
+  <p class="text-xl">
+    Roller Derby er en fullkontaktsport på rulleskøyter, og vi er hovedstadens
+    klubb. Vi har nybegynnerkurs og en plass til alle. Om du vil være spiller,
+    dommer med eller uten hjul. Bli med oss da vel!
+  </p>
+  <a href="/somewhere"> Høres spennede ut? Finn ut mer! </a>
+</section>
