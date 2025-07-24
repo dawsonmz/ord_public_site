83ea9f9e7b5834a475aee6b45c1724a25a2db86b
its-steff <steffdziadkowiec@proton.me>
2025-07-24 13:38:11 +0200

    refactor to css

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -3,7 +3,7 @@
   import instaIcon from "$lib/assets/insta.png";
 </script>
 
-<footer class="flex justify-between">
+<footer>
   <p>&copy; 2023 Oslo Roller Derby Klubb</p>
   <div class="flex">
     <a aria-label="link to facebook" href="/">
@@ -14,3 +14,16 @@
     </a>
   </div>
 </footer>
+
+<style>
+  footer {
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 20px 10px 20px;
+
+    a:first-child {
+      margin-right: 10px;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,57 +4,41 @@
   import sponserCode from "$lib/assets/GrasrotQR.png";
 </script>
 
-<section
-  id="hero_section"
-  class="px-10 xl:px-0 flex flex-col lg:flex-row lg:gap-4 justify-between lg:max-w-[1090px]"
->
-  <img
-    class="hidden lg:block lg:w-100"
-    src={hero}
-    alt="Skater in uniform with a helmet"
-  />
-  <div class="flex flex-col gap-6 mt-10 lg:mt-25">
-    <h1 class="text-4xl lg:text-6xl text-white leading-tight font-bold">
+<section class="hero_section">
+  <img src={hero} alt="Skater in uniform with a helmet" />
+  <div>
+    <h1>
       RÅ<br />
       INKLUDERENDE<br />
       VELORGANISERTE<br />
     </h1>
-    <p class="text-white text-lg lg:text-xl max-w-[586px]">
+    <p>
       Roller Derby er en fullkontaktsport på rulleskøyter, og vi er hovedstadens
       klubb. Vi har nybegynnerkurs og en plass til alle. Om du vil være spiller,
       dommer med eller uten hjul.
       <br /><br />
       Bli med oss da vel!
     </p>
-    <a
-      class="text-black p-6 text-lg mt-4 rounded-md text-center max-w-[420px]"
-      style="background-color: #FFCB4D;"
-      href="/about"
-    >
-      Høres spennede ut? Finn ut mer!
-    </a>
+    <a href="/about"> Høres spennede ut? Finn ut mer! </a>
   </div>
 </section>
 
-<section
-  class="my-14 text-black lg:flex justify-between px-10 lg:px-30 py-4 lg:py-10"
-  style="background-color: #FFCB4D;"
->
+<section class="event_section">
   <div>
-    <h2 class="text-2xl lg:text-4xl">Eventkalender</h2>
+    <h2>Eventkalender</h2>
   </div>
   <div>
-    <img class="hidden lg:block" src={ohOslo} alt="CTA text Oslo team chant" />
+    <img src={ohOslo} alt="CTA text Oslo team chant" />
   </div>
 </section>
 
-<section class="text-white mx-10 mb-14">
-  <div class="flex flex-col lg:flex-row gap-9">
-    <div class="flex gap-4">
+<section class="sponsor_section">
+  <div class="sponsor_container">
+    <div>
       <img src={sponserCode} alt="QR code to sponsor Oslo Roller Derby" />
       <div>
-        <h3 class="text-2xl">Støtt oss med din grasrotandel</h3>
-        <p class="mt-2">
+        <h3>Støtt oss med din grasrotandel</h3>
+        <p>
           Du kan bli grasrotgiver i alle Norsk Tippings kanaler eller ved å
           sende SMS “Grasrotandelen 997503465” til 600000
         </p>
@@ -62,12 +46,11 @@
     </div>
 
     <div>
-      <h3 class="text-2xl">Vil du bli sponsor?</h3>
-      <p class="mt-2">
+      <h3>Vil du bli sponsor?</h3>
+      <p>
         Ønsker du å støtte klubben og samtidig få med logo på kamper og andre
         arrangementer? Send oss en mail til <a
-          href="mailto:spons@oslorollerderby.no"
-          class="text-[#EF7A33]">spons@oslorollerderby.no</a
+          href="mailto:spons@oslorollerderby.no">spons@oslorollerderby.no</a
         >
       </p>
     </div>
@@ -75,7 +58,117 @@
 </section>
 
 <style>
-  #hero_section {
-    margin: 0 auto;
+  .hero_section {
+    margin: 80px 20px 80px 20px;
+    color: white;
+
+    div {
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+    }
+
+    img {
+      display: none;
+    }
+
+    h1 {
+      font-size: 50px;
+      line-height: 1.3;
+      font-weight: 700;
+    }
+
+    p {
+      font-size: 16px;
+    }
+
+    a {
+      background-color: #ffcb4d;
+      color: black;
+      padding: 20px 10px;
+      border-radius: 6px;
+      text-align: center;
+      margin-top: 40px;
+    }
+
+    @media (min-width: 1024px) {
+      display: flex;
+      justify-content: center;
+      gap: 60px;
+      /* margin-left: 150px;
+      margin-right: 150px; */
+      img {
+        display: block;
+        width: 400px;
+      }
+
+      h1 {
+        font-size: 60px;
+      }
+
+      div {
+        margin-top: 110px;
+        max-width: 500px;
+      }
+    }
+  }
+
+  .event_section {
+    background-color: #ffcb4d;
+    color: black;
+    font-size: 42px;
+    font-weight: 700;
+    height: 50vh;
+    padding: 50px;
+    margin-bottom: 80px;
+
+    img {
+      display: none;
+    }
+
+    @media (min-width: 1024px) {
+      display: flex;
+      justify-content: space-between;
+      img {
+        display: block;
+      }
+    }
+  }
+
+  .sponsor_section {
+    margin: 0 20px 80px 20px;
+    color: white;
+
+    .sponsor_container {
+      display: flex;
+      flex-direction: column;
+      gap: 40px;
+
+      h3 {
+        font-size: 25px;
+      }
+
+      div:first-child {
+        display: flex;
+        gap: 20px;
+        justify-content: center;
+      }
+
+      div:last-child {
+        a {
+          color: #f08435;
+        }
+      }
+    }
+
+    @media (min-width: 1024px) {
+      margin-left: 80px;
+      margin-right: 80px;
+
+      .sponsor_container {
+        flex-direction: row;
+        gap: 80px;
+      }
+    }
   }
 </style>
